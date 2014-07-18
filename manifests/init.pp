e3e6251c1f1f2e45e54d2e261e33684d07eee452
Adam Breaux <breauxaj@gmail.com>
2014-07-18 13:01:33 -0400

    added docs

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,31 @@
+# Class: duplicity
+#
+# This class installs duplicity
+#
+# Parameters:
+#
+#  ensure: (default latest)
+#    Determine the state of the packages
+#
+# Actions:
+#   - Installs the duplicity package
+#
+# Requires:
+#
+# Sample Usage:
+#
+#  For a standard installation, use:
+#
+#    class { 'duplicity':
+#      ensure => 'latest'
+#    }
+#
+#  To remove the installation, use:
+#
+#    class { 'duplicity':
+#      ensure => 'absent'
+#    }
+#
 class duplicity (
   $ensure = 'latest'
 ){
