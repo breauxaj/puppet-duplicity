08d704bafc91475679fcbeb716ab4dfe1ec5f3a9
Adam Breaux <breauxaj@gmail.com>
2014-07-18 13:08:56 -0400

    added docs

--- manifests/job.pp
+++ manifests/job.pp
@@ -1,3 +1,55 @@
+# Define: duplicity::job
+#
+# This define constructs a schedule job running duplicity
+#
+# Parameters:
+#
+#  source:
+#    Source location
+#
+#  destination:
+#    Destination location
+#
+#  options:
+#    Options to be passed to duplicity command
+#
+#  runas:
+#    User that should run the job
+#
+#  minute:
+#    Cron time interval
+#
+#  hour:
+#    Cron time interval
+#
+#  monthday:
+#    Cron time interval
+#
+#  month:
+#    Cron time interval
+#
+#  weekday:
+#    Cron time interval
+#
+# Actions:
+#   - Creates a job from the parameters passed
+#   - Schedules the job to run based on parameters
+#
+# Requires:
+#
+#  EPEL repository
+#
+# Sample Usage:
+#
+#  To configure a job, use:
+#
+#    duplicity::job { 'backup':
+#      source      => '/home/source',
+#      destination => '/home/destination',
+#      runas       => 'deploy',
+#      minute      => '0',
+#    }
+#
 define duplicity::job (
   $source = '',
   $destination = '',
